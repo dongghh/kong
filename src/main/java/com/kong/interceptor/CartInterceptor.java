cccfffed905d0689c3839564c8d48ffd9a986379
dongghh <dong3405@gmail.com>
2022-12-22 03:26:14 +0900

    장바구니,주문 접근권한 수정

---
+++ src/main/java/com/kong/interceptor/CartInterceptor.java
@@ -0,0 +1,29 @@
+package com.kong.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kong.domain.MemberVO;
+
+public class CartInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object obj) throws Exception {
+
+		HttpSession session = req.getSession();
+		MemberVO member = (MemberVO) session.getAttribute("member");
+
+		if (member == null) {
+			res.sendRedirect("/");
+			return false;
+		}
+
+		
+
+		return true;
+	}
+
+}
